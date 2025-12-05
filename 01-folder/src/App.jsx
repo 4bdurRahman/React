13a797768e1ffec63372abe3b78fe93c3c25b939
4bdurRahman <justme.ar7@gmail.com>
2025-12-05 12:31:41 +0500

    01-folder commit done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <h2>Name:</h2>
+      <p>     Abdur rahman</p>
+      <h2>Age:</h2>
+      <p>     23</p>
+      <h2>Address:</h2>
+      <p>     Dhaka, Pakistan</p>
+    </div>
+  )
+}
+
+export default App
